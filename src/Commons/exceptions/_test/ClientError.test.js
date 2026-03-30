9c410c047fd2e29fc74fea872b9b75369393d64c
Ezyh Dzikron Arofi <ezyh.arofi@gmail.com>
2026-03-31 06:31:48 +0700

    first commit

---
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import ClientError from "../ClientError.js";
+
+describe("ClientError", () => {
+  it("should throw error when directly use it", () => {
+    expect(() => new ClientError("")).toThrowError(
+      "cannot instantiate abstract class",
+    );
+  });
+});
